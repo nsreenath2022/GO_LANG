7f6882a1bfb961970d88428fcf36cbc1151dfde1
nsreenath2022 <nsreenath2022@gmail.com>
2023-10-05 15:03:48 +0530

    control statemenetsgo run /Users/nsreenath2001/go_projects/LEARN-GO-ONE/control_flow/control_flow_excercise.go

--- control_flow/control_flow_excercise.go
+++ control_flow/control_flow_excercise.go
@@ -153,6 +153,7 @@ func excercise_seven(){
 	
 }
 
+
 func main () {
 	//excercise_one()
 	//excercise_two()
@@ -160,5 +161,133 @@ func main () {
 	//excercise_four()
 	//excercise_five()
 	//excercise_six()
-	excercise_seven()
+	//excercise_seven()
+	// excercise_eight()
+	// excercise_nine()
+	// excercise_ten()
+
+	// excercise_nine_a()
+	// excercise_nested_loop()
+	// excercise_map()
+
+	//excercise_comma_ok()
+
+	//excercise_statement_statement()
+	excercise_print_boolean_keys()
+}
+
+func excercise_print_boolean_keys(){
+	fmt.Println("true && true ==>", true && true)
+	fmt.Println("true && false ==>", true && false)
+	fmt.Println("true || true ==>", true || true)
+	fmt.Println("true || false ==>", true|| false)
+	fmt.Println("!true ==>", !true)
+}
+
+func excercise_statement_statement(){
+	cnt := 0
+	for i := 0; i < 100; i++{
+		if x := rand.Intn(5); x == 3{
+			fmt.Println("x is 3")
+			cnt++
+		}
+	}
+	fmt.Println("count = ", cnt)
+}
+
+func excercise_comma_ok() {
+	mp := map [string]int {
+		"James": 42,
+		"MoneyPenny": 32,
+	}
+
+	for key,val := range mp {
+		fmt.Printf("name = %v and age = %v\n", key, val)
+	}
+
+	age := mp["James"]
+	fmt.Println("James age = ", age)
+
+	age1 := mp["Q"]
+	fmt.Println("Q's age = ", age1)
+
+	// comma ok is used to check if item present in map
+	if val, ok := mp["James"]; ok{
+		fmt.Println("James is present and here is val = ", val)
+	} else {
+		fmt.Println(" James is not present")
+	}
+
+	if _, check := mp["Q"]; check{
+		fmt.Println("Q is present")
+	} else {
+		fmt.Println("Q is not present")
+	}
+}
+
+func excercise_map(){
+	mp := map [string]int {
+		"James": 42,
+		"MoneyPenny": 32,
+	}
+
+	for key,val := range mp {
+		fmt.Printf("name = %v and age = %v\n", key, val)
+	}
+
+	james_age := mp["James"]
+	fmt.Println("James age = ", james_age)
+}
+
+func excercise_nested_loop() {
+	fmt.Println("------------------------------------")
+	for i := 0; i < 5; i++{
+		for j := 0; j < 5; j++{
+			fmt.Printf("outer line = %v and onner line = %v\n", i, j)
+		}
+		fmt.Println("------------------------------------")
+	}
+}
+
+// infinate loop
+func excercise_eight() {
+	i := 25
+	for i > -8{
+		fmt.Println(i)
+		i--
+	}
+}
+
+
+func excercise_nine_a() {
+	count := 100
+	for {
+		if count < 0 {
+			break;
+		} else if count % 2 == 0 {
+			count--
+			continue
+		}
+		
+		fmt.Printf("%d\n", count)
+		count--;
+	}
+}
+func excercise_nine() {
+	i := 25
+	for {
+		if i == 15{
+			break
+		}
+		fmt.Println(i)
+		i--
+	}
+}
+
+func excercise_ten() {
+	xi := [] int {42, 43, 44, 45, 46, 47}
+
+	for i, v := range xi{
+		fmt.Printf(" index = %v and value = %v\n", i, v)
+	}
 }
